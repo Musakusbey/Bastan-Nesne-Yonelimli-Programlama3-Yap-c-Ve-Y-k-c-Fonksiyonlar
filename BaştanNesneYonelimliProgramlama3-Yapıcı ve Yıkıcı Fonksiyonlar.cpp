7cc866a9abfa43914e1a9c999758769f67faedd7
Musa Kuşbey <78588618+Musakusbey@users.noreply.github.com>
2021-03-07 01:46:03 +0300

    Add files via upload
    Bastan Nesne  Yonelimli Programlama3-Yapıcı Ve Yıkıcı Fonksiyonlar

---
+++ BaştanNesneYonelimliProgramlama3-Yapıcı ve Yıkıcı Fonksiyonlar.cpp
@@ -0,0 +1,73 @@
+/*#include<iostream>
+
+using namespace std;
+
+class deneme
+{
+	public:
+		
+		int a,b,c;
+		
+	deneme();
+	
+}test;
+
+deneme::deneme()
+{
+	a=1;
+	
+	b=2;
+	
+	c=3;
+}
+
+int main ()
+{
+	cout<<test.a<<endl<<test.b<<endl<<test.c<<endl;
+	
+	return 0;
+}  */
+
+/*#include<iostream>
+
+using namespace std;
+
+class deneme
+{
+	int a;
+	
+	public:
+		
+	deneme(int x)
+	{
+		a=x;
+	}
+	
+	void goster()
+	{
+		cout<<a<<endl;
+	}
+};
+
+int main ()
+{
+	deneme test(15);
+	
+	test.goster();
+	
+	return 0;
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
